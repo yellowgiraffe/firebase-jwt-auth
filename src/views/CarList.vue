55cf442655b078043ceaf3217425430ef7225e92
Olga Mirocz <ol.miroch@gmail.com>
2023-07-05 18:34:28 +0200

    add car list and axios interceptor

--- src/views/CarList.vue
+++ src/views/CarList.vue
@@ -1,3 +1,36 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+import Card from 'primevue/card'
+import LoaderSpinner from '../components/LoaderSpinner.vue'
+
+const cars = ref();
+const isLoading = ref(false)
+
+const getCarList = async () => {
+  try {
+    isLoading.value = true
+    const response = await axios.get(`https://my-app-466e8-default-rtdb.europe-west1.firebasedatabase.app/cars.json`)
+    cars.value = response.data
+  } catch (err) {
+    console.log(err.response)
+  } finally {
+    isLoading.value = false
+  }
+}
+
+onMounted( async () => {
+  await getCarList()
+})
+</script>
 <template>
   <h2>Cars</h2>
+  <LoaderSpinner v-if="isLoading" />
+  <div v-else class="flex flex-column gap-3">
+    <Card v-for="(car, i) in cars" :key="i">
+      <template #title>{{ car.brand }}</template>
+      <template #subtitle>{{ car.model }}</template>
+    </Card>
+  </div>
 </template>
